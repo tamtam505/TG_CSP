f0947941d68c163d5405b606fc37f6d88c1faa84
tamtam505 <tamara.garcia@ucas-edu.net>
2026-09-14 14:37:59 -0600

    variables_practice.py

---
+++ logical_memory.py
@@ -0,0 +1,9 @@
+# TG, Logical and Physical memory notes
+
+pi = 3.141 592
+
+school = "UCAS"
+
+print(f"The variable pi has a value of{pi} and is saved at {id(pi)}")
+
+print(f"The variable school has a value of {school} and is saved at {id(school)}")
--- variables_practice.py
+++ variables_practice.py
@@ -2,8 +2,40 @@
 
 name= "Tamara"
 
-a_number_bettwen_one_and_ten ="5" 
+print(name)
+
+a_number_between_one_and_ten ="5" 
+
+print(a_number_between_one_and_ten)
 
 a_number_between_hundred_and_thousand ="200"
 
-what_you_ate_for_breakfest = 
+print(a_number_between_hundred_and_thousand)
+
+what_you_ate_for_breakfest = "doughnut" 
+
+print(what_you_ate_for_breakfest)
+
+what_is_your_favorite_color = "pink"
+
+print(what_is_your_favorite_color)
+
+a_school_name = "UCAS"
+
+print(a_school_name)
+
+the_year = "2026"
+
+print(the_year)
+
+your_eye_color = "brown"
+
+print(your_eye_color)
+
+someones_age = "14"
+
+print(someones_age)
+
+your_favorite_subject_in_school = "Intro to health science"
+
+print(your_favorite_subject_in_school)
