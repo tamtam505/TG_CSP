7bd14db39f2a532d85c4d13b1ec6ca0daf990ece
tamtam505 <tamara.garcia@ucas-edu.net>
2026-09-22 14:54:06 -0600

    conditionals notes.py

---
+++ silly_sentences.py
@@ -0,0 +1,10 @@
+# TG, 7th Silly sentences
+
+adjective = input("Tell me an adjective:")
+adjective = input("Tell me an adjecitve:")
+bird = input( "Tell me type of bird:")
+room = input("Tell me a room in the house:")
+verb = input("Tell me a past tense verb:") 
+name = input("Tell me a name")
+
+             
---
+++ your_budget.py
@@ -0,0 +1,51 @@
+# TG, your budget
+
+# while true loop
+
+while True: 
+    try:
+       income = float(input("What is your monthly income?:"))
+       break 
+    except:
+            print("you forgot to type your monthly income")
+            
+# while true loop
+
+while True: 
+    try:
+       rent = float(input("What is your monthly rent/mortage?:"))
+       break 
+    except:
+            print("you forgot to type your monthly rent/mortage")
+
+
+# while true loop
+
+while True: 
+    try:
+       utilities  = float(input("What is your monthly utilities?:"))
+       break 
+    except:
+            print("you forgot to type your monthly utilities")   
+
+            
+# while true loop
+
+while True: 
+    try:
+       groceres = float(input("What is your monthly groceries?:"))
+       break 
+    except:
+            print("you forgot to type your monthly groceries")
+
+        
+# while true loop
+
+while True: 
+    try:
+       transportation = float(input("What is your monthly transportation?:"))
+       break 
+    except:
+            print("you forgot to type your monthly transportation")
+
+
